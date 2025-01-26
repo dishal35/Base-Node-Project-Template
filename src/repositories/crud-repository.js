c6c3b2056b36c92f8634388129bcc075daca6301
dishal35 <ldisha1103@gmail.com>
2025-01-27 00:36:14 +0530

    Basic Crud

---
+++ src/repositories/crud-repository.js
@@ -0,0 +1,66 @@
+const {Logger} =require('../config')
+class CrudRepository{
+    constructor(model){
+        this.model=model
+    }
+    async create(data){
+        try{
+        const response=await this.model.create(data);
+        return response;
+        }
+        catch(error){
+            Logger.error("Crud Repo Error:create")
+            throw error;
+        }
+    }
+    async destroy(data){
+        try{
+        const response=await this.model.destroy({
+            where:{
+                id:data
+            }
+        });
+        return response;
+        }
+        catch(error){
+            Logger.error("Crud Repo Error:destroy")
+            throw error;
+        }
+    }
+    async get(data){
+        try{
+        const response=await this.model.findbyPk(data)
+        return response;
+        }
+        catch(error){
+            Logger.error("Crud Repo Error:get")
+            throw error;
+        }
+    }
+    async getAll(){
+        try{
+        const response=await this.model.findAll()
+        return response;
+        }
+        catch(error){
+            Logger.error("Crud Repo Error:get")
+            throw error;
+        }
+    }
+    async update(id,data){
+        try{
+        const response=await this.model.update(data,{
+            where:{
+                id:id
+            }
+        })
+        return response;
+        }
+        catch(error){
+            Logger.error("Crud Repo Error:Update")
+            throw error;
+        }
+    }
+
+}
+module.exports=CrudRepository
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 const { InfoController } = require('../../controllers');
+const airplaneRoutes=require('./airplane-route');
 
 const router = express.Router();
-
+router.use('/airplane',airplaneRoutes)
 router.get('/info', InfoController.info);
 
 module.exports = router;
