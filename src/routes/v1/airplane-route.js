5a94cfe6429741ede8b0fe50b4d76582caf80330
dishal35 <ldisha1103@gmail.com>
2025-03-06 01:40:59 +0530

    added routes

--- src/routes/v1/airplane-route.js
+++ src/routes/v1/airplane-route.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const { AirplaneController } = require('../../controllers');
+const { AirplaneMiddleware } = require('../../middlewares');
+
 const router = express.Router();
 
-router.post('/airplanes', AirplaneController.createAirplane);
+// Create an airplane
+router.post('/', AirplaneMiddleware.validateCreateRequest, AirplaneController.createAirplane);
+
+// Get all airplanes
+router.get('/', AirplaneController.getAllAirplanes);
+
+// Get a specific airplane by ID
+router.get('/:id', AirplaneController.getAirplane);
+
+// Update an airplane by ID
+router.put('/:id', AirplaneMiddleware.validateUpdateRequest, AirplaneController.updateAirplane);
+
+// Delete an airplane by ID
+router.delete('/:id', AirplaneController.deleteAirplane);
 
 module.exports = router;
