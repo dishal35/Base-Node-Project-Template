8bcb9021abd5c47e7107c2c8f7cdd70b168fbea7
dishal35 <ldisha1103@gmail.com>
2025-03-05 23:40:32 +0530

    created services

--- src/services/airplane-service.js
+++ src/services/airplane-service.js
@@ -1,4 +1,6 @@
+const { StatusCodes } = require("http-status-codes");
 const { AirplaneRepository } = require("../repositories");
+const { AppError } = require("../utils/errors/app-error");
 const airplanerepository=new AirplaneRepository();
 async function createAirplane(data){
     try{
@@ -6,8 +8,61 @@ async function createAirplane(data){
         return airplane;
     }
     catch(error){
+        if(error.name=='TypeError'){
+            throw new AppError('Cannot Create Airplane',StatusCodes.BAD_REQUEST)
+        }
+        throw error;
+    }
+}
+async function getAirplane(id) {
+    try {
+        const airplane = await airplanerepository.get(id);
+        if (!airplane) {
+            throw new AppError('Airplane not found', StatusCodes.NOT_FOUND);
+        }
+        return airplane;
+    } catch (error) {
         throw error;
     }
 }
 
-module.exports={createAirplane};
+async function getAllAirplanes() {
+    try {
+        const airplanes = await airplanerepository.getAll();
+        return airplanes;
+    } catch (error) {
+        throw new AppError('Error retrieving airplanes', StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
+
+async function updateAirplane(id, data) {
+    try {
+        const updatedAirplane = await airplanerepository.update(id, data);
+        if (updatedAirplane[0] === 0) {
+            throw new AppError('Airplane not found or no changes made', StatusCodes.NOT_FOUND);
+        }
+        return updatedAirplane;
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function deleteAirplane(id) {
+    try {
+        const deleted = await airplanerepository.destroy(id);
+        if (deleted === 0) {
+            throw new AppError('Airplane not found', StatusCodes.NOT_FOUND);
+        }
+        return deleted;
+    } catch (error) {
+        throw error;
+    }
+}
+
+module.exports = {
+    createAirplane,
+    getAirplane,
+    getAllAirplanes,
+    updateAirplane,
+    deleteAirplane
+  };
